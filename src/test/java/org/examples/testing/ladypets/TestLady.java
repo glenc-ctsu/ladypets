1d0450c7d5a09cdafc3de060f8695d63fadcf81b
glen ctsu <guanguo.cui@ctsu.ox.ac.uk>
2016-03-22 15:27:52 +0000

    Exception practice ...

--- src/test/java/org/examples/testing/ladypets/TestLady.java
+++ src/test/java/org/examples/testing/ladypets/TestLady.java
@@ -1,6 +1,7 @@
 package org.examples.testing.ladypets;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 //import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
@@ -15,6 +16,30 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestLady {
+	public String readPropertyFromFile(String keyName) throws IOException{
+		String workingDirectory = System.getProperty("user.dir");
+		//System.out.println(workingDirectory);
+		Properties prop = new Properties();
+		String propertyFilePath = workingDirectory + "/src/test/java/org/examples/testing/resources/";
+		FileInputStream fis=null;
+		try {
+			fis = new FileInputStream(propertyFilePath + "Param.properties");
+		} catch (FileNotFoundException e) {
+			System.out.println("Property file not found !");
+			e.printStackTrace();
+		}
+
+		try {
+			prop.load(fis);
+		} catch (IOException e) {
+			System.out.println("Property file loading failure !");
+			e.printStackTrace();
+		}
+		return prop.getProperty(keyName);
+	}
+	
+
+
 	@BeforeMethod
 	public void printMethodNameBefore(Method method){
 		System.out.println("Test Name: " + method.getName() + " -- Starts @ " + new SimpleDateFormat("E yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
@@ -26,14 +51,15 @@ public class TestLady {
 	}
 	
 	@Test
-	public void testPlayPets() throws IOException{
-		String workingDirectory = System.getProperty("user.dir");
-		//System.out.println(workingDirectory);
-		Properties prop = new Properties();
-		String propertyFilePath = workingDirectory + "/src/test/java/org/examples/testing/resources/";
-		prop.load(new FileInputStream( propertyFilePath + "Param.properties"));
-		String countString = prop.getProperty("LadyCount");
-		int ladyCount = Integer.parseInt(countString);
+	public void testPlayPets(){
+		String keyValue=null;
+		try {
+			keyValue = readPropertyFromFile("LadyCount");
+		} catch (IOException e) {
+			System.out.println("Failed to ge keyValue !!!");
+			e.printStackTrace();
+		}
+		int ladyCount = Integer.parseInt(keyValue);
 		System.out.println(" Total lady count = " + ladyCount); //read it from "Para.properties" file
 		DataFactory df =new DataFactory(); //Fake name generator
 		for(int i=0; i < ladyCount; i++){
